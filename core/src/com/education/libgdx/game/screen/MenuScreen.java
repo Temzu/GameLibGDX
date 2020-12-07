1dd4829d081932e9b6e04ad72cd51915346cc006
Temzu <40913z@mail.ru>
2020-12-07 13:01:36 +0300

    completed task

--- core/src/com/education/libgdx/game/screen/MenuScreen.java
+++ core/src/com/education/libgdx/game/screen/MenuScreen.java
@@ -9,13 +9,19 @@ import com.education.libgdx.game.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
     private Texture img;
-
+    private Vector2 posObj;
+    private Vector2 posTouch;
+    private Vector2 vVector;
+    private Vector2 buf;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-
+        posObj = new Vector2();
+        posTouch = new Vector2();
+        vVector = new Vector2();
+        buf = new Vector2();
     }
 
     @Override
@@ -24,10 +30,13 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.54f, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
+        buf.set(posTouch);
+        if (buf.sub(posObj).len() > 10f) {
+            posObj.add(vVector);
+        }
 
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(img, posObj.x, posObj.y);
         batch.end();
     }
 
@@ -39,19 +48,21 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-
+        posTouch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        vVector.set(posTouch.cpy().sub(posObj)).setLength(10f);
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-
+        posTouch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        vVector.set(posTouch.cpy().sub(posObj)).setLength(10f);
         return super.touchDragged(screenX, screenY, pointer);
     }
 
     @Override
     public boolean keyDown(int keycode) {
-        
+
         return super.keyDown(keycode);
     }
 }
