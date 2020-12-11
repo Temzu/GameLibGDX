1b6ac0dfa06def5330fd6305e439a62aacda3769
Temzu <40913z@mail.ru>
2020-12-11 18:20:52 +0300

    completed hw_3

--- core/src/com/education/libgdx/game/screen/MenuScreen.java
+++ core/src/com/education/libgdx/game/screen/MenuScreen.java
@@ -7,13 +7,14 @@ import com.badlogic.gdx.math.Vector2;
 import com.education.libgdx.game.base.BaseScreen;
 import com.education.libgdx.game.math.Rect;
 import com.education.libgdx.game.sprite.Background;
+import com.education.libgdx.game.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
-
     private Texture img;
+    private Logo logo;
     private Vector2 pos;
 
     @Override
@@ -23,16 +24,20 @@ public class MenuScreen extends BaseScreen {
         background = new Background(bg);
         img = new Texture("badlogic.jpg");
         pos = new Vector2();
+        logo = new Logo(img);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+
+
+
         Gdx.gl.glClearColor(0.55f, 0.23f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
-        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -46,10 +51,18 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return super.touchDown(touch, pointer, button);
+    }
+
+    @Override
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        logo.touchDragged(touch, pointer);
+        return super.touchDragged(touch, pointer);
     }
 }
